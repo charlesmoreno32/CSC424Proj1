3ade939f85da99165eb9926ddfed49e591d509c4
charlesmoreno32 <charlesmoreno007@gmail.com>
2024-01-29 23:15:13 -0800

    reformatted

--- react-frontend/src/context/AuthProvider.js
+++ react-frontend/src/context/AuthProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffectgit  } from "react";
+import { createContext, useContext, useState, useEffect  } from "react";
 import { useNavigate } from "react-router-dom";
 import {checkCookie} from "../apis.js";
 
